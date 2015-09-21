6ec3ececfa06d8ce61363c57c682fc1c4be1923c
Brad Lindsay <lindsay@polleverywhere.com>
2015-09-21 11:03:19 -0400

    Refine sub_pages api and add max_depth option
    Instead of passing either a string or a regular expression as the first
    argument, require the first argument to be a string of the relative path
    whose sub pages we're interested in. A regular expression can now be
    passed to the "filter" option instead to further filter the results.
    
    Change the default sort to be first by depth and then alphabetical. Also
    got rid of the ability to pass a sort block. The default sorting should
    cover > 90% of use cases, and the ability to pass that block just
    removes having to type ".sort_by" in front of the block they would be
    passing. I think the code is clearer forcing them to type actually use
    the "sort_by" method.
    
    Add a "max_depth" option to ignore results that are deeper than the
    value passed to max_depth.

--- lib/middleman/contrib/helpers.rb
+++ lib/middleman/contrib/helpers.rb
@@ -2,18 +2,32 @@ module Middleman
   module Contrib
     
     module Helpers
-      def sub_pages(dir_path, &sort)
-        sort ||= proc {}
-        
-        selector = case dir_path
-        when String
-          dir_path = "#{dir_path.chomp '/'}/"
-          proc { |resource| resource.path.start_with?(dir_path) }
-        when Regexp
-          proc { |resource| resource.path =~ dir_path }
+      def sub_pages(dir_path, opts={})
+        opts     = opts.dup
+        dir_path = dir_path.chomp('/')
+        dir_path = "#{dir_path}/" if dir_path != ''
+
+        opts[:filter] ||= //
+        opts[:max_depth] += (dir_path.count('/') - 1) if opts[:max_depth]
+
+        resources = sitemap.resources.select do |resource|
+          path = resource.path
+
+          path.start_with?(dir_path) && path =~ opts[:filter] &&
+          (!opts[:max_depth] || path.count('/') <= opts[:max_depth])
         end
+
+        resources.sort_by { |resource| [resource.path.count('/'), resource.path] }
         
-        sitemap.resources.select(&selector).sort_by(&sort)
+        # selector = case dir_path
+        # when String
+        #   dir_path = "#{dir_path.chomp '/'}/"
+        #   proc { |resource| resource.path.start_with?(dir_path) }
+        # when Regexp
+        #   proc { |resource| resource.path =~ dir_path }
+        # end
+        
+        # sitemap.resources.select(&selector).sort_by(&sort)
       end 
     end
 
--- spec/middleman/helpers_spec.rb
+++ spec/middleman/helpers_spec.rb
@@ -6,22 +6,65 @@ describe Middleman::Contrib::Helpers do
   end
   
   describe 'sub_pages' do
-    it 'returns an array of pages in the specified folder, recursively digging through sub-folders' do
+    it 'returns an array of pages in the specified folder and any subfolders, sorted shallowist pages first and then alphabetically' do
       paths = app.sub_pages('topic').map(&:path)
       
-      expect(paths).to eq ['topic/sub_a/sub_sub_a.html', 'topic/sub_a/sub_sub_b.html', 'topic/sub_a.html', 'topic/sub_b.html']
-    end
-
-    it 'returns an array of pages that match the specified regular expression' do
-      paths = app.sub_pages(/\Atopic\/.*a.html.*\z/).map(&:path)
-
-      expect(paths).to eq ['topic/sub_a/sub_sub_a.html', 'topic/sub_a.html']
-    end
-
-    it 'returns an array of pages in a folder in the order specified by the block' do
-      paths = app.sub_pages('topic') { |page| [page.path.count('/'), page.path] }.map(&:path)
-
       expect(paths).to eq ['topic/sub_a.html', 'topic/sub_b.html', 'topic/sub_a/sub_sub_a.html', 'topic/sub_a/sub_sub_b.html']
+    end
+
+    it 'returns only the sub pages that match the specified regular expression' do
+      paths = app.sub_pages('topic', filter: /\A.*a.html.*\z/).map(&:path)
+
+      expect(paths).to eq ['topic/sub_a.html', 'topic/sub_a/sub_sub_a.html']
+    end
+
+    it 'takes in a relative path from source and returns an array of pages found below it' do
+      paths = app.sub_pages('topic/sub_a').map(&:path)
+
+      expect(paths).to eq ['topic/sub_a/sub_sub_a.html', 'topic/sub_a/sub_sub_b.html']
+    end
+
+    
+    context 'passing an empty string for the directory path' do
+      it 'returns a full listing of all the files' do
+        # Just testing that it returns more than 0 items
+        expect(app.sub_pages('').size).to be > 0
+      end
+
+      it 'filters results by matching the specified regular expression' do
+        paths = app.sub_pages('', filter: /\Atopic\/.*a.html.*\z/).map(&:path)
+
+        expect(paths).to eq ['topic/sub_a.html', 'topic/sub_a/sub_sub_a.html']
+      end
+    end
+
+
+    context 'passing "/" for the directory path' do
+      it 'returns a full listing of all the files' do
+        # Just testing that it returns more than 0 items
+        expect(app.sub_pages('/').size).to be > 0
+      end
+
+      it 'filters results by matching the specified regular expression' do
+        paths = app.sub_pages('/', filter: /\Atopic\/.*a.html.*\z/).map(&:path)
+
+        expect(paths).to eq ['topic/sub_a.html', 'topic/sub_a/sub_sub_a.html']
+      end
+    end
+
+
+    context 'passed the "max_depth" option' do
+      it 'limits the results to those items no deeper than the specified value' do
+        paths = app.sub_pages('topic', max_depth: 1).map(&:path)
+
+        expect(paths).to eq ['topic/sub_a.html', 'topic/sub_b.html']
+      end
+
+      it 'calculates the maximum depth relative to the directory path passed' do
+        paths = app.sub_pages('topic/sub_a', max_depth: 1).map(&:path)
+
+        expect(paths).to eq ['topic/sub_a/sub_sub_a.html', 'topic/sub_a/sub_sub_b.html']
+      end
     end
   end
 end
